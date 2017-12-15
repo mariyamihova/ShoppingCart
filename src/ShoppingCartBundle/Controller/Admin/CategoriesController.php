c884ff71a582c6c98a10e5659a6070adc9e61f01
mimihova <mimihova@gmail.com>
2017-12-15 09:44:28 +0200

    admin/editor - Products and Categories Controllers added

---
+++ src/ShoppingCartBundle/Controller/Admin/CategoriesController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace ShoppingCartBundle\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use ShoppingCartBundle\Entity\Category;
+use ShoppingCartBundle\Form\AddEditCategoryForm;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class CategoriesController
+ * @package ShoppingCartBundle\Controller\Admin
+ *
+ * @Route("/admin")
+ * @Security("is_granted(['ROLE_EDITOR','ROLE_ADMIN'])")
+ *
+ */
+class CategoriesController extends Controller
+{
+    /**
+     * @Route("/categories", name="admin_view_categories")
+     * @Method("GET")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
+    public function viewAllAction()
+    {
+        $categories=$this->getDoctrine()->getRepository(Category::class)->findAll();
+
+        return $this->render("admin/categories/list.html.twig",["categories"=>$categories]);
+    }
+
+    /**
+     * @Route("/categories/add", name="admin_add_category")
+     * @Method({"GET", "POST"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
+    public function addCategoryAction(Request $request)
+    {
+
+        $category=new Category();
+        $category->setImageName("classical.jpg");
+
+        $form=$this->createForm(AddEditCategoryForm::class,$category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            $this->addFlash("success", "Category {$category->getName()} was added!");
+            return $this->redirectToRoute("admin_view_categories");
+        }
+
+        return $this->render("admin/categories/add.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/categories/edit/{id}", name="admin_edit_category")
+     * @Method({"GET", "POST"})
+     *
+     * @param Request $request
+     * @param Category $category
+     * @return Response
+     */
+
+    public function editCategoryAction(Request $request,Category $category)
+    {
+
+        $form = $this->createForm(AddEditCategoryForm::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            $this->addFlash("success", "Category {$category->getName()} was updated!");
+            return $this->redirectToRoute("admin_view_categories");
+        }
+
+        return $this->render("admin/categories/edit.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/categories/delete/{id}", name="admin_delete_category")
+     * @Method("POST")
+     *
+     * @param Category $category
+     * @return Response
+     */
+
+    public function deleteCategoryAction(Category $category)
+    {
+        if ($category->getProducts()->count() > 0)
+        {
+
+            $this->addFlash("danger", "Category with products in it cannot be deleted.");
+            return $this->redirectToRoute("admin_view_categories");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($category);
+        $em->flush();
+
+        $this->addFlash("success", "Category {$category->getName()} was deleted!");
+        return $this->redirectToRoute("admin_view_categories");
+    }
+}
---
+++ src/ShoppingCartBundle/Controller/Admin/ProductsController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace ShoppingCartBundle\Controller\Admin;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use ShoppingCartBundle\Entity\Product;
+use ShoppingCartBundle\Form\AddEditProductForm;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/**
+ * Class ProductsController
+ * @package ShoppingCartBundle\Controller\Admin
+ *
+ * @Route("/admin")
+ * @Security("is_granted(['ROLE_EDITOR','ROLE_ADMIN'])")
+ */
+class ProductsController extends Controller
+{
+    /**
+     * @Route("/products", name="admin_list_products")
+     * @Method("GET")
+     *
+     * @return Response
+     */
+    public function listProductsAction()
+    {
+
+        $products = $this->getDoctrine()->getRepository(Product::class)
+                ->findAll();
+        return $this->render("admin/products/list.html.twig",
+            ["products" =>$products]);
+    }
+
+    /**
+     * @Route("/products/add", name="admin_add_product")
+     * @Method({"GET", "POST"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function addProductAction(Request $request)
+    {
+        $product=new Product();
+
+        $form = $this->createForm(AddEditProductForm::class,$product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+
+            $product->setUpdatedAt(new \DateTime());
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash("success", "Product {$product->getName()} added successfully.");
+            return $this->redirectToRoute("admin_list_products");
+        }
+
+        return $this->render("admin/products/add.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/products/edit/{id}", name="admin_edit_product")
+     * @Method({"GET", "POST"})
+     *
+     * @param Request $request
+     * @param Product $product
+     * @return Response
+     */
+
+    public function editProductAction(Request $request, Product $product)
+    {
+        $form = $this->createForm(AddEditProductForm::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash("success", "Product {$product->getName()} edited successfully.");
+            return $this->redirectToRoute("admin_list_products");
+        }
+
+        return $this->render("admin/products/edit.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/products/delete/{id}", name="admin_delete_product")
+     * @Method("POST")
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function deleteProductAction(Product $product)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+
+        $this->addFlash("success", "Product {$product->getName()} was deleted successfully.");
+        return $this->redirectToRoute("admin_list_products");
+    }
+}
