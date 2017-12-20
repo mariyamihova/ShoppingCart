2791f1b8117331fff9ffde7414ab7d6197ae3579
mimihova <mimihova@gmail.com>
2017-12-20 20:29:11 +0200

    some usings have been removed

--- src/ShoppingCartBundle/Form/ProductSellType.php
+++ src/ShoppingCartBundle/Form/ProductSellType.php
@@ -2,11 +2,9 @@
 
 namespace ShoppingCartBundle\Form;
 
-use ShoppingCartBundle\Entity\Product;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
